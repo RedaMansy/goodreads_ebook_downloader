133c2b05ee146ee2d9754d057040a2e3b332a102
RedaMansy <ridamansy@gmail.com>
2024-04-02 20:45:47 +0100

    added caching and download link fetching

--- gr_libgen.py
+++ gr_libgen.py
@@ -8,10 +8,6 @@ cached_titles = {}
 def extract_book_titles(url):
 
     try:
-
-        if url in cached_titles:
-            print("from cache")
-            return cached_titles[url]
         
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for bad status codes
@@ -30,6 +26,7 @@ def extract_book_titles(url):
             titles.append(title_text)
 
         cached_titles[title_text] = titles
+        print(cached_titles)
 
         return titles
     except requests.exceptions.RequestException as e:
@@ -39,17 +36,46 @@ def extract_book_titles(url):
 titles = extract_book_titles(url)
 print(titles)
     
+cached_links = {}
 
 def search_for_book(titles):
     zlib = "https://singlelogin.re/s/"
     # zlib = requests.get(zlib)
     # zlib.raise_for_status()
-    
-    for i in titles:
-        search_string = zlib + i
-        response = requests.get(search_string)
-        soup = BeautifulSoup(response.content, "html.parser")
-        links = [a["href"] for a in soup.select('td.itemCover a')]
-        print(links[0])
-    
-search_for_book(titles)
+    if titles in cached_links:
+        return cached_links[titles]
+    # for i in titles:
+    search_string = zlib + titles
+    response = requests.get(search_string)
+    soup = BeautifulSoup(response.content, "html.parser")
+    links = [a["href"] for a in soup.select('td.itemCover a')][0]
+    cached_links[titles] = links
+    print(links)
+    return links
+
+book_link = search_for_book("Dune")    
+
+def fetch_html_from_url(book_link):
+    try:
+        url = "https://singlelogin.re/s/"
+        book_link = url + book_link
+        response = requests.get(book_link)
+        response.raise_for_status()
+        print("Response: ", response.text)
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL: {e}")
+        return None
+
+
+def download_epub(html_content):
+    content = fetch_html_from_url(html_content)
+    if content:
+        soup = BeautifulSoup(content, 'html.parser')
+        download_link = soup.find('a', {'data-target-desktop': 'new-tab'})
+        if download_link:
+            return download_link['href']
+        else:
+            return None
+
+download_epub("/book/5757113/e330dc/frank-herberts-dune-saga-collection-books-1-6.html")
